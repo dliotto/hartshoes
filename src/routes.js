ddf3274acf4876d10ce82c8706debee8fb5d3ebd
Douglas Hartmann Liotto <douglas.liotto@dwanke.local>
2020-03-03 17:29:15 -0300

    Criando interações entre página inicial e carrinho de compras com redux

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
-import Main from './pages/Main';
-import Respository from './pages/Repository';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function Routes() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Main} />
-                <Route path="/repository/:repository" component={Respository} />
-            </Switch>
-        </BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/cart" component={Cart} />
+        </Switch>
     );
 }
 
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
+import background from '../assets/images/background.svg';
+
 export default createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
  * {
      margin: 0;
      padding: 0;
@@ -13,18 +18,24 @@ export default createGlobalStyle`
  }
 
  body{
-     background: #7159c1;
+     background: #0f4c81 url(${background}) no-repeat center top;
      -webkit-font-smoothing: antialiased !important;
  }
 
  body, input, button {
-     color: #222;
+     color: #fff;
      font-size: 14px;
-     font-family: Arial, Helvetica, sans-serif;
+     font-family: Roboto, sans-serif;
  }
 
  button {
      cursor: pointer;
+ }
+
+ #root {
+     max-width: 1020px;
+     margin: 0 auto;
+     padding: 0 20px 50px;
  }
 
 `;
